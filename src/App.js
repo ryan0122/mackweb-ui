54cf7eb1e3aebf376a50786a504add505fb856ea
ryan0122 <ryan.mack1200@gmail.com>
2017-08-01 16:17:11 -0600

    added navigation and material-ui stuff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import About from './modules/About'
+import Contact from './modules/Contact'
+import Home from './modules/Home'
 
-class App extends Component {
-  render() {
-    return (
+const App = () => (
+<MuiThemeProvider>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>MackWeb UI</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact}/>
+        </div>
+      </Router>
       </div>
-    );
-  }
-}
+</MuiThemeProvider>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Needed for material-ui
+// https://github.com/zilverline/react-tap-event-plugin
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
+ReactDOM.render((
+  <Router basename="/">
+    <App/>
+  </Router>
+),document.getElementById('root'));
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
